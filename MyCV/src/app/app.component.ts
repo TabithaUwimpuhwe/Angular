847b53682b9027ecde240d39166f3ef0dae431cf
PrincessTabitha <tabithauwimpuhwe@gmail.com>
2018-04-30 09:08:11 -0400

    initial commit

---
+++ MyCV/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { //Content info of the HTML section
+//  title = 'Name:';
+//   email = 'email'; 
+//    phonenumber = 'phone';
+//    company = 'p&g'; 
+//    education = 'CSU';
+}
---
+++ MyCV/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WorkExperienceComponent,
+    ContactComponent,
+    EducationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
